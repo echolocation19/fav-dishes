06024666b2bf407f6701a296f5480fad9427437e
echolocation19 <sakur2420@mail.ru>
2022-03-23 14:49:05 +0300

    init commit

---
+++ app/src/main/java/com/example/favdishes/ui/SplashActivity.kt
@@ -0,0 +1,66 @@
+package com.example.favdishes.ui
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.example.favdishes.MainActivity
+import com.example.favdishes.R
+import com.example.favdishes.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private val binding: ActivitySplashBinding by lazy {
+        ActivitySplashBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        hideNotificationBar()
+        loadAnimation()
+    }
+
+    private fun isVersionHigherThanR() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.R
+
+    private fun hideNotificationBar() {
+        if (isVersionHigherThanR()) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+    }
+
+    private fun loadAnimation() {
+        val splashAnim = AnimationUtils.loadAnimation(this, R.anim.anim_splash)
+        binding.tvAppName.animation = splashAnim
+        splashAnim.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(p0: Animation?) {}
+
+            override fun onAnimationEnd(p0: Animation?) {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(MainActivity.newActivity(this@SplashActivity))
+                    finish()
+                }, HANDLER_DELAY)
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {}
+
+        })
+    }
+
+    companion object {
+        private const val HANDLER_DELAY = 1000L
+    }
+
+
+}
